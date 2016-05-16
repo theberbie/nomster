8bab7596e810488179d94cd20e106d72f446480f
vagrant <vagrant@precise32.(none)>
2016-05-16 02:32:50 +0000

    Added controller comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,16 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+
+    @place = Place.find(params[place_id])
+    @place.comment.create(comments_params.merge(user: current_user))
+    redirect_to place_path(@place)
+
+  end
+
+  private
+  def comments_params 
+    params.require(:comment).permit(:message, :rating)
+  end
 end
