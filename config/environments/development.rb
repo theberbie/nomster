e12e104e4821752601aa6402749b5fb73bac7c5c
vagrant <vagrant@precise32.(none)>
2016-05-21 05:11:54 +0000

    Added email trigger

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,16 +31,16 @@ Nomster::Application.configure do
   # number of complex assets.
   config.assets.debug = true
 
-  config.action_mailer.delivery_method = :smtp
+  #config.action_mailer.delivery_method = :smtp
 
-  config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: "example.com",
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: ENV['GMAIL_ADDRESS'],
-    password: ENV['GMAIL_PASSWORD']
-  }
-  
-end
+  #config.action_mailer.smtp_settings = {
+   # address: "smtp.gmail.com",
+   # port: 587,
+   # domain: "example.com",
+   # authentication: "plain",
+  #  enable_starttls_auto: true,
+   # user_name: ENV['GMAIL_ADDRESS'],
+ #  # password: ENV['GMAIL_PASSWORD']
+ # }
+
+#
