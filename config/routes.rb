a0f49e188bf42ca74889510580a28dcfcdd8dd09
vagrant <vagrant@precise32.(none)>
2016-05-22 03:00:14 +0000

    created user profile

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,10 @@ Nomster::Application.routes.draw do
   resources :places do
     resources :comments, :only =>:create
     resources :photos
+
   end
+
+  resources :users, only: :show
 
 
   # The priority is based upon order of creation: first created -> highest priority.
